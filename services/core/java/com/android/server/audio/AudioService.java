64e829f581b3a5817fb7295151ea5080fbcd8e05
Amit Pundir <amit.pundir@linaro.org>
2013-07-27 01:44:32 +0530

    frameworks/base: Fix AudioService JNI leaks
    Root cause: AudioService keeps creating new instance of
    ForceControlStreamClient and linkToDeath with the same
    callback which leads to global reference table overflow
    Fix: don't create new instance if call back doesn't change;
    release old instance before create new one

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -1874,7 +1874,16 @@ public class AudioService extends IAudioService.Stub
                 }
                 mUserSelectedVolumeControlStream = false;
             } else {
-                mForceControlStreamClient = new ForceControlStreamClient(cb);
+                if (null == mForceControlStreamClient) {
+                    mForceControlStreamClient = new ForceControlStreamClient(cb);
+                } else {
+                    if (mForceControlStreamClient.getBinder() == cb) {
+                        Log.d(TAG, "forceVolumeControlStream cb:" + cb + " is already linked.");
+                    } else {
+                        mForceControlStreamClient.release();
+                        mForceControlStreamClient = new ForceControlStreamClient(cb);
+                    }
+                }
             }
         }
     }
@@ -1913,6 +1922,10 @@ public class AudioService extends IAudioService.Stub
                 mCb.unlinkToDeath(this, 0);
                 mCb = null;
             }
+        }
+
+        public IBinder getBinder() {
+            return mCb;
         }
     }
 
