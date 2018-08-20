b9b22307dd485765f277e2881452d4cab4dd0c4e
Shilin Victor <chrono.monochrome@gmail.com>
2018-08-20 22:57:43 +0300

    MountService: catch IllegalStateException for getField
    An IllegalStateException will be thrown when getFiled is timeout.
    So catch it to avoid crash.

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -1029,6 +1029,8 @@ class StorageManagerService extends IStorageManager.Stub
         String systemLocale;
         try {
             systemLocale = getField(StorageManager.SYSTEM_LOCALE_KEY);
+        } catch (IllegalStateException e) {
+            return;
         } catch (RemoteException e) {
             return;
         }
