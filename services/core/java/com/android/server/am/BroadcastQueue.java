5225195335d25b14fa220aa3477d02b05ed385cd
Shilin Victor <chrono.monochrome@gmail.com>
2018-08-20 22:57:43 +0300

    clean up pending broadcast receviers when force stop package
    when app was force-stop, only clean up mOrderedBroadcasts and mParallelBroadcasts,
    maybe exist broadcast receviers in pendind list, need clean up it completely.
    
    AMS.forceStopPackage
        AMS.forceStopPackageLocked
            cleanupDisabledPackageReceiversLocked

--- services/core/java/com/android/server/am/BroadcastQueue.java
+++ services/core/java/com/android/server/am/BroadcastQueue.java
@@ -1560,6 +1560,14 @@ public final class BroadcastQueue {
             }
         }
 
+        if (mPendingBroadcast != null) {
+            didSomething |= mPendingBroadcast.cleanupDisabledPackageReceiversLocked(
+                    packageName, filterByClasses, userId, doit);
+            if (!doit && didSomething) {
+                return true;
+            }
+        }
+
         return didSomething;
     }
 
