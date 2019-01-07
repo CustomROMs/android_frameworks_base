4e6f30758a8b316095f4c4e61d6ea621a04523de
Shilin Victor <chrono.monochrome@gmail.com>
2019-01-08 00:46:41 +0300

    Extras: Add dynamic theme fonts support for O
    Change-Id: I69bf5b11c0ce102b85b09aa7e76ad602cbb5e4ea

--- core/java/android/os/Process.java
+++ core/java/android/os/Process.java
@@ -469,10 +469,11 @@ public class Process {
                                   String instructionSet,
                                   String appDataDir,
                                   String invokeWith,
-                                  String[] zygoteArgs) {
+                                  String[] zygoteArgs,
+                                  boolean refreshFont) {
         return zygoteProcess.start(processClass, niceName, uid, gid, gids,
                     debugFlags, mountExternal, targetSdkVersion, seInfo,
-                    abi, instructionSet, appDataDir, invokeWith, zygoteArgs);
+                    abi, instructionSet, appDataDir, invokeWith, zygoteArgs, refreshFont);
     }
 
     /** @hide */
@@ -486,10 +487,11 @@ public class Process {
                                   String instructionSet,
                                   String appDataDir,
                                   String invokeWith,
-                                  String[] zygoteArgs) {
+                                  String[] zygoteArgs,
+                                  boolean refreshFont) {
         return WebViewZygote.getProcess().start(processClass, niceName, uid, gid, gids,
                     debugFlags, mountExternal, targetSdkVersion, seInfo,
-                    abi, instructionSet, appDataDir, invokeWith, zygoteArgs);
+                    abi, instructionSet, appDataDir, invokeWith, zygoteArgs, refreshFont);
     }
 
     /**
